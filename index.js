5fa77085be99894354887635bad12abf3e2230ea
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-03-02 20:00:56 -0300

    basic functions

---
+++ index.js
@@ -0,0 +1,55 @@
+const discord = require("discord.js");
+require("dotenv/config");
+const client = new discord.Client();
+
+const token = process.env.TOKEN;
+
+client.on("ready", () => {
+	console.log(`bot ta rodando como ${client.user.tag}`);
+});
+
+client.on("message", (msg) => {
+	const args = msg.content.split(" ");
+	if (args[0] === "pomodoro") {
+		// const args = msg.content.substring(PREFIX.length).split(" ");
+		console.log(args);
+		const voiceChannel = msg.member.voice.channel;
+		const members = voiceChannel.members;
+
+			msg.reply("partiu dxar de ser vagabundo");
+			const workingTime = args[1] * 60000;
+			// const restTime = args[2];
+
+			voiceChannel.join().then((connection) => {
+				const rounds = args[3];
+				muteLoop(members, workingTime, restTime, rounds);
+			});
+		}
+	}
+});
+
+async function muteLoop(members, workingTime, restTime, rounds) {
+	for (let i = 0; i < rounds; i++) {
+		// const dispatcher = connection.play('./startAudio.mp3');
+		console.log("começou o tempo");
+		for (let [key, guildMember] of members) {
+			if (!guildMember.user.bot) {
+				guildMember.voice.setMute(true);
+			}
+		}
+		await sleep(workingTime);
+		for (let [key, guildMember] of members) {
+			if (!guildMember.user.bot) {
+				guildMember.voice.setMute(false);
+			}
+		}
+		await sleep(restTime);
+		// const dispatcher = connection.play('./startAudio.mp3');
+		// dispatcher.on("end", end => {
+		// 	voiceChannel.leave();
+		// });
+	}
+}
+
+function sleep(ms) {
+client.login(token);
