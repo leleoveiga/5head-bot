9de0d30641537a0208e2919fe8fe6c52d8d3ffc1
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-03-03 11:05:10 -0300

    refactoring

--- index.js
+++ index.js
@@ -12,42 +12,41 @@ client.on("message", (msg) => {
 	const args = msg.content.split(" ");
 	if (args[0] === "pomodoro") {
 		// const args = msg.content.substring(PREFIX.length).split(" ");
-		console.log(args);
-		const voiceChannel = msg.member.voice.channel;
-		const members = voiceChannel.members;
-
-		if (args[1] === "ajuda") {
-			client.reply(
-				"só escrever: pomodoro x y z\n onde x: minutos trabalhando\n y: minutos descansando \n z: rounds"
-			);
-		}
-
-		if (args[1] === "sai") {
-			for (let [key, guildMember] of members) {
-				if (!guildMember.user.bot) {
-					guildMember.voice.setMute(false);
-				}
-			}
-			voiceChannel.leave();
-		}
-		if (
-			parseInt(args[1]) > 0 &&
-			parseInt(args[2]) > 0 &&
-			parseInt(args[3]) > 0
-		) {
-			msg.reply("partiu dxar de ser vagabundo");
-			const workingTime = args[1] * 60000;
-			const restTime = args[2] * 60000;
-			// const workingTime = args[1];
-			// const restTime = args[2];
-
-			voiceChannel.join().then((connection) => {
-				const rounds = args[3];
-				muteLoop(members, workingTime, restTime, rounds);
-			});
-		}
+		pomodoro(args, msg);
 	}
 });
+
+function pomodoro(args, msg) {
+	console.log(args);
+	const voiceChannel = msg.member.voice.channel;
+	const members = voiceChannel.members;
+
+	if (args[1] === "ajuda") {
+		client.reply(
+			"só escrever: pomodoro x y z\n onde x: minutos trabalhando\n y: minutos descansando \n z: rounds"
+		);
+	}
+
+	if (args[1] === "sai") {
+		for (let [key, guildMember] of members) {
+			if (!guildMember.user.bot) {
+				guildMember.voice.setMute(false);
+			}
+		}
+		voiceChannel.leave();
+	}
+	if (parseInt(args[1]) > 0 && parseInt(args[2]) > 0 && parseInt(args[3]) > 0) {
+		msg.reply(
+			`partiu dxar de ser vagabundo. \n${args[1]} minutos trabalhando\n${args[2]} minutos descansando\n${args[3]} rounds`
+		);
+		const workingTime = args[1] * 60000;
+		const restTime = args[2] * 60000;
+		const rounds = args[3];
+		voiceChannel.join().then((connection) => {
+			muteLoop(members, workingTime, restTime, rounds);
+		});
+	}
+}
 
 async function muteLoop(members, workingTime, restTime, rounds) {
 	for (let i = 0; i < rounds; i++) {
